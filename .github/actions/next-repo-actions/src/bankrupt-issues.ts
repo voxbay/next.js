9e2528e95480c723cfe9b2762bd34d3bb44e341e
samcx <sam@vercel.com>
2024-06-05 16:25:23 -0700

    chore(github-workflow): update dist

--- .github/actions/next-repo-actions/src/bankrupt-issues.ts
+++ .github/actions/next-repo-actions/src/bankrupt-issues.ts
@@ -1,3 +1,23 @@
-import { info } from '@actions/core'
+import { context, getOctokit } from '@actions/github'
+import { info, setFailed } from '@actions/core'
 
-info('Hello, world!')
+async function main() {
+  if (!process.env.GITHUB_TOKEN) throw new TypeError('GITHUB_TOKEN not set')
+
+  const octokit = getOctokit(process.env.GITHUB_TOKEN)
+  const { owner, repo } = context.repo
+
+  try {
+    const { data: issue } = await octokit.rest.issues.get({
+      owner,
+      repo,
+      issue_number: 66573,
+    })
+
+    info(`Issue data = ${JSON.stringify(issue)}`)
+  } catch (error) {
+    setFailed(error)
+  }
+}
+
+main()
